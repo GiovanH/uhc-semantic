6a575ff1084bf9c574773e36792ffec8d78ac662
Gio <sethg@ipi.org>
2023-10-10 21:53:25 -0500

    Fix sprite^2 matching, edge cases, hide known errors

--- mod.js
+++ mod.js
@@ -3,12 +3,35 @@ var semantic_info;
 var dom;
 var cache = {};
 
-const re_convo_line_with_speaker = /^<span style="[^"]*?color: #(?<color>[A-Fa-f0-9]+)"[^>]*?>(?<label>\??[()A-Za-z0-9_^]{2,50})[<:]/
+const re_convo_line_with_speaker = /^<span style="[^"]*?color: #(?<color>[A-Fa-f0-9]+)"[^>]*?>(?<label>\??(The )?[()A-Za-z0-9_^]{2,50})[<:]/
 const re_convo_line_no_speaker   = /^<span style="[^"]*?color: #(?<color>[A-Fa-f0-9]+)"[^>]*?>(?!(CURRENT|PAST|FUTURE|\?\?\?)?[a-z]+[A-Z][a-z]+ )(?!\[[A-Z]{2}\])/
+
+const ignore_errors_in = [
+  "009348 footnotes", // mspa reader
+  "008993 log second", // oilretcon
+  "007907 log firstpass", // text
+  "007632 log second no_speaker", // hic
+  "006870 footnotes", // text
+  "005796 log", // tumor
+  "005771 log", // text
+  "007163 footnotes", // SILENCE fx
+  "005609 log second no_speaker", // text
+  "005138 log second no_speaker", // typewriter cartridge
+  "004942 log second no_speaker", // tumor debris
+  "004823 log firstpass with_speaker", // tumor debris
+  "002149 footnotes", // gamefaqs
+]
+
+function maybeError() {
+  const jobnote = arguments[0]
+  if (!ignore_errors_in.some(m => jobnote.startsWith(m))) {
+    api.logger.error(...arguments)
+  }
+}
 
 const simpleHash = str => {
   let hash = 0;
-  for (const i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     const char = str.charCodeAt(i);
     hash = (hash << 5) - hash + char;
     hash &= hash; // Convert to 32bit integer
@@ -16,22 +39,23 @@ const simpleHash = str => {
   return new Uint32Array([hash])[0].toString(36);
 };
 
-function apiStoreMemoized(funkey, fun){
-  return function(content_str, options) {
-    return fun(content_str, options)
+// function apiStoreMemoized(funkey, fun){
+//   return function(content_str, options) {
+//     // return fun(content_str, options)
 
-    // cache[funkey] = cache[funkey] || {}
-    // const argkey = simpleHash(content_str)
+//     cache[funkey] = cache[funkey] || {}
+//     const argkey = simpleHash(content_str)
 
-    // const cached_result = cache[funkey][argkey]
-    // if (cached_result) return cached_result
-    // else {
-    //   const result = fun(content_str, options)
-    //   cache[funkey][argkey] = result
-    //   return result
-    // }
-  }
-}
+//     const cached_result = cache[funkey][argkey]
+//     if (cached_result) return cached_result
+//     else {
+//       // (api.logger || console).warn("cache miss", content_str)
+//       const result = fun(content_str, options)
+//       cache[funkey][argkey] = result
+//       return result
+//     }
+//   }
+// }
 
 function labelPredicateFactory(labels) {
   const all_matchers = labels.map(v => [
@@ -71,11 +95,11 @@ function naiveInnerText(node) {
 }
 
 const person_matchers = [
-  { name: "andrew", predicate(node) {
+  { name: "hussie", predicate(node) {
     if (node.getAttribute('data-sem-color') == '000000' && node.getAttribute('data-sem-logtype') == 'authorlog') {
       return true
     }
-    return labelColorPairPredicateFactory(["HUSSIE"], '000000');
+    return labelColorPairPredicateFactory(["HUSSIE"], '000000')(node);
   }},
   { name: "aradia", predicate: labelColorPairPredicateFactory(["AA", "ARADIASPRITE", "ARADIABOT", "ARADIA"], 'A10000') },
   { name: "aradia.bot", predicate: labelColorPairPredicateFactory(["AA"], '000056') },
@@ -97,18 +121,19 @@ const person_matchers = [
   { name: "dave.sprite", predicate: labelColorPairPredicateFactory(["DAVESPRITE", "TG"], 'F2A400') },
   { name: "dirk", predicate: labelColorPairPredicateFactory(["TT", "DIRK"], ['F2A400', 'FFCC00']) },
   { name: "dirk.alt", predicate: labelColorPairPredicateFactory(["TT", "DIRK"], ['E00707']) },
-  { name: "dirk.alt.equius.sprite", predicate: labelPredicateFactory(["ARQUIUSPRITE"]) },
-  { name: "dirk.equius.gamzee.docscratch", predicate: labelColorPairPredicateFactory(["undefined"], 'FFFFFF') },
+  { name: "dirk.alt.equius.sprite", tag: "arquiusprite2", predicate: labelPredicateFactory(["ARQUIUSPRITE"]) },
+  { name: "dirk.equius.gamzee.docscratch", tag: "doc_scratch", predicate: labelColorPairPredicateFactory(["undefined"], 'FFFFFF') },
   { name: "equius", predicate: labelColorPairPredicateFactory(["CT", "EQUIUS"], '000056') },
   { name: "equius.sprite", predicate: labelPredicateFactory(["EQUIUSPRITE"]) },
   { name: "eridan", predicate: labelColorPairPredicateFactory(["CA", "ERIDAN"], '6A006A') },
   { name: "eridan.sollux.sprite", predicate: labelPredicateFactory(["ERISOLSPRITE"]) },
   { name: "feferi", predicate: labelPredicateFactory(["CC", "FEFERI"]) },
-  { name: "feferi.nepeta.sprite", predicate: labelPredicateFactory(["FEFETASPRITE"]) },
+  { name: "feferi.nepeta.sprite", tag: "fefetasprite", predicate: labelPredicateFactory(["FEFETASPRITE"]) },
   { name: "gamzee", predicate: labelPredicateFactory(["TC", "GAMZEE"]) },
-  { name: "gcat.tavros.sprite", predicate: labelPredicateFactory(["GCATAVROSPRITE"]) },
+  { name: "gcat.tavros.sprite", tag: "gcatavrosprite", predicate: labelPredicateFactory(["GCATAVROSPRITE"]) },
 
   { name: "dad", predicate: labelColorPairPredicateFactory(["pipefan413"], ['4B4B4B']) },
+  { name: "dd", predicate: labelPredicateFactory(["The Dignitary"]) },
 
   { name: "hatfans", predicate(node) {
     return [
@@ -117,36 +142,36 @@ const person_matchers = [
     ].includes(node.getAttribute('data-sem-label'))
   }},
   { name: "jade", predicate: labelColorPairPredicateFactory(["GG", "JADE"], '4AC925') },
-  { name: "jade.sprite", predicate: labelPredicateFactory(["JADESPRITE"]) },
+  { name: "jade.sprite", tag: "jadesprite", predicate: labelPredicateFactory(["JADESPRITE"]) },
   { name: "jade.grimbark", predicate: labelColorPairPredicateFactory(["JADE", "BARK"], '000000') },
   { name: "jake", predicate: labelColorPairPredicateFactory(["JAKE", "GT"], '1F9400') },
   { name: "jane", predicate: labelColorPairPredicateFactory(["GG", "JANE"], '00D5F2') },
   { name: "jane.crockertier", predicate: labelColorPairPredicateFactory(["JANE"], 'FF0000') },
-  { name: "jane.sprite", predicate: labelColorPairPredicateFactory(["NANNASPRITE", "NANNASPRITEx2", "pipefan413"], '00D5F2') },
-  { name: "jasper.rose.sprite", predicate: labelPredicateFactory(["JASPROSESPRITE^2"]) },
+  { name: "jane.sprite", tag: "nannasprite", predicate: labelColorPairPredicateFactory(["NANNASPRITE", "NANNASPRITEx2", "pipefan413"], '00D5F2') },
+  { name: "rose.jasper.sprite", tag: "jasprosesprite2",  predicate: labelPredicateFactory(["JASPROSESPRITE^2"]) },
   { name: "jasper.sprite", predicate: labelPredicateFactory(["JASPERSPRITE"]) },
-  { name: "john", predicate: labelColorPairPredicateFactory(["EB", "JOHN", "GT", "(JOHN)", "E8", "CEB"], '0715CD') },
+  { name: "john", predicate: labelColorPairPredicateFactory(["EB", "E8", "JOHN", "GT", "(JOHN)", "E8", "CEB"], '0715CD') },
   { name: "kanaya", predicate: labelColorPairPredicateFactory(["GA", "CGA", "FGA", "KANAYA"], '008141') },
-  { name: "kanayamom", predicate: labelPredicateFactory(["MOTHERSPRITE"]) },
+  { name: "kanayamom", tag: "lusus",  predicate: labelPredicateFactory(["MOTHERSPRITE"]) },
   { name: "karkat", predicate: labelColorPairPredicateFactory(["CG", "KARKAT", "KARKATs"], ['FF0000', '626262']) },
   { name: "meenah", predicate: labelPredicateFactory(["MEENAH"]) },
   { name: "nepeta", predicate: labelColorPairPredicateFactory(["AC", "NEPETA"], '416600') },
   { name: "nepeta.sprite", predicate: labelPredicateFactory(["NEPETASPRITE"]) },
-  { name: "rose", predicate: labelColorPairPredicateFactory(["TT", "ROSE"], 'B536DA') },
+  { name: "rose", predicate: labelColorPairPredicateFactory(["TT", "ROSE"], ['B536DA', '9A61AC']) },
   { name: "rose.sprite", predicate: labelPredicateFactory(["ROSESPRITE"]) },
   { name: "roxy", predicate: labelColorPairPredicateFactory(["TG", "ROXY", "undefined"], ['FBBAFF', 'FF6FF2']) },
   { name: "sollux", predicate: labelColorPairPredicateFactory(["TA", "SOLLUX"], 'A1A100') },
   { name: "tavros", predicate: labelColorPairPredicateFactory(["AT", "TAVROS"], 'A15000') },
   { name: "tavros.sprite", predicate: labelPredicateFactory(["TAVROSPRITE"]) },
-  { name: "tavros.vriska.sprite", predicate: labelPredicateFactory(["TAVRISPRITE"]) },
+  { name: "tavros.vriska.sprite", tag: "tavrisprite",  predicate: labelPredicateFactory(["TAVRISPRITE"]) },
   { name: "terezi", predicate: labelColorPairPredicateFactory(["GC", "TEREZI", "TER3Z1"], ['008282', 'E00707']) },
-  { name: "terezimom", predicate: labelPredicateFactory(["DRAGONSPRITE"]) },
+  { name: "terezimom", tag: "lusus",  predicate: labelPredicateFactory(["DRAGONSPRITE"]) },
   { name: "vriska", predicate: labelColorPairPredicateFactory(["AG", "VRISKA", "(VRISKA)"], '005682') },
 
   { name: "cronus", predicate: labelPredicateFactory(["CRONUS"]) },
   { name: "damara", predicate: labelPredicateFactory(["DAMARA"]) },
   { name: "horuss", predicate: labelPredicateFactory(["HORUSS"]) },
-  { name: "hussie", predicate: labelPredicateFactory(["HUSSIE"]) },
+  // { name: "hussie", predicate: labelPredicateFactory(["HUSSIE"]) },
   { name: "kankri", predicate: labelPredicateFactory(["KANKRI"]) },
   { name: "kurloz", predicate: labelPredicateFactory(["KURLOZ"]) },
   { name: "latula", predicate: labelPredicateFactory(["LATULA"]) },
@@ -157,6 +182,17 @@ const person_matchers = [
   { name: "rufioh", predicate: labelPredicateFactory(["RUFIOH"]) },
   { name: "squarewave", predicate: labelPredicateFactory(["SQUAREWAVE"]) },
   { name: "salamander", predicate: labelPredicateFactory(["SALAMANDER", "MERCHANT", "HABERDASHER"], ['000000']) },
+
+  { name: "", predicate(node) {
+    // common known styles
+    const text = node.innerText.toUpperCase()
+    return (
+      text.includes("TUMOR")
+      || text == "L"
+      || text == "RD ENGLISH"
+      || text.length == 1
+    )
+  }},
 ]
 
 function identifyPerson(node, guess, jobnote) {
@@ -170,7 +206,8 @@ function identifyPerson(node, guess, jobnote) {
     }
   }
   // if (res) return res
-  api.logger.error(jobnote, "Unknown person", node.outerHTML, guess)
+
+  maybeError(jobnote, "Unknown person", node.outerHTML, guess)
   // return guess
 }
 
@@ -195,7 +232,8 @@ function newSemanticInfo() {
   }
 }
 
-const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(orig_content, options) {
+// const makeSemanticContent = apiStoreMemoized('makeSemanticContent',
+const makeSemanticContent = (function(orig_content, options) {
   const page_num = options.page
   // Optimization: skip image-only pages
   if (!orig_content.trim()) {
@@ -207,9 +245,9 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
 
   options = options || {}
 
-  const { JSDOM } = require('./node_modules/jsdom')
+  // const { JSDOM } = require('./node_modules/jsdom')
   // if (!dom) api.logger.warn("Creating new DOM")
-  dom = dom || new JSDOM('<!DOCTYPE html><body></body>').window.document;
+  dom = dom || document.implementation.createHTMLDocument("Sandbox") // new JSDOM('<!DOCTYPE html><body></body>').window.document;
 
   container = dom.createElement('div')
   container.innerHTML = orig_content
@@ -226,7 +264,9 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
       semantic_info.all_people[person].conversations.add(page_num)
       semantic_info.all_people[person].labels.add(label)
       semantic_info.all_people[person].colors.add(node.getAttribute(`data-sem-color`))
+      return true
     }
+    return false
   }
 
   function processLinesWithMatcher(re_matcher, querySelector, jobnote) {
@@ -243,6 +283,9 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
       try {
         const match = re_matcher.exec(loh)
         if (match) {
+          // Debug
+          line.setAttribute(`data-sem-orig`, line.outerHTML)
+
           const label = match.groups['label']
           line.setAttribute('data-sem-label', label)
           line.setAttribute('data-sem-color', match.groups['color'].toUpperCase())
@@ -252,14 +295,18 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
           // Check for spr2/crockertier/grimbark and process next span
           const real_line = line.nextElementSibling
           const extra_space = line.nextSibling
+          const next_is_own_line = real_line && (real_line.innerText.includes(":") && re_convo_line_with_speaker.exec(real_line.outerHTML))
           if (
             re_matcher == re_convo_line_with_speaker         // We are searching for labels, not partial text
             && !(extra_space && extra_space.nodeType == line.TEXT_NODE && extra_space.textContent.trim())          // If next element is plaintext, it's just a space
-            && label && (label.length + 10 > line.textContent.length)   // We found a label, and we are (approx, davepeta) just a label
+            && label // We found a label,
+            //  && (label.length + 10 > line.textContent.length)   // and we are (approx, davepeta) just a label
             && real_line && real_line.matches('span[style]')         // The next element sibling is a styled span
-            && !re_convo_line_with_speaker.exec(real_line.outerHTML) // Next element is not a valid label (009432)
+            && !(next_is_own_line) // Next element is not a labeled line (009432)
           ) {
+            // Debug
             // real_line.setAttribute(`data-sem-leapfrog`, "target")
+            real_line.setAttribute(`data-sem-orig`, real_line.outerHTML)
             // line.setAttribute(`data-sem-leapfrog`, "jumper")
 
             // Recompute line based on combined information (our label, next color)
@@ -269,11 +316,11 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
               // api.logger.debug(`Replacing our color ${match.groups['color']} with their color ${next_line_match.groups['color']}`)
               line.setAttribute('data-sem-color', next_line_match.groups['color'].toUpperCase())
             } else {
-              api.logger.error(jobnote, `Real line didn't match color regex\n`, real_line.outerHTML)
+              // maybeError(jobnote, `Real line didn't match color regex\n`, real_line.outerHTML)
             }
 
             // Retry with color info
-            tryIdAndSetPerson(line, label, jobnote)
+            let rl_matched = tryIdAndSetPerson(line, label, jobnote)
 
             // Hoist label into span
             if (extra_space && extra_space.nodeType == line.TEXT_NODE)
@@ -283,6 +330,9 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
 
             // Copy person from label to real line (don't reprocess it)
             real_line.setAttribute('data-sem-person', line.getAttribute('data-sem-person'))
+          } else if (next_is_own_line) {
+            // Next line might need processing
+            real_line.setAttribute('data-sem-doubleline', true)
           } else {
             // shrug
           }
@@ -303,12 +353,13 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
     container.querySelectorAll('span[style]').forEach(line => {
       line.setAttribute(`data-sem-logtype`, text_type)
     })
-    processLinesWithMatcher(re_convo_line_no_speaker, 'span[style]', 'authorlog no_speaker')
+    processLinesWithMatcher(re_convo_line_no_speaker, 'span[style]', `${page_num} authorlog no_speaker`)
   } else if (text_type == 'log') {
-    processLinesWithMatcher(re_convo_line_with_speaker, 'br + span[style], span[style]:first-child', 'log firstpass with_speaker')
-    processLinesWithMatcher(re_convo_line_no_speaker, 'span[style]:not([data-sem-person])', 'log second no_speaker')
+    processLinesWithMatcher(re_convo_line_with_speaker, 'br + span[style], span[style]:first-child', `${page_num} log firstpass with_speaker`)
+    processLinesWithMatcher(re_convo_line_with_speaker, 'span[data-sem-doubleline]', `${page_num} log doublelalonde with_speaker`)
+    processLinesWithMatcher(re_convo_line_no_speaker, 'span[style]:not([data-sem-person])', `${page_num} log second no_speaker`)
   } else if (options.crawl_footnotes) {
-    processLinesWithMatcher(re_convo_line_with_speaker, 'span[style]', 'footnotes')
+    processLinesWithMatcher(re_convo_line_with_speaker, 'span[style]', `${page_num} footnotes`)
   }
 
   // if (res != orig_content) {
@@ -327,16 +378,18 @@ const makeSemanticContent = apiStoreMemoized('makeSemanticContent', function(ori
   return res
 })
 
+const NOCACHE = true
+
 module.exports = {
   title: "Semantic Info",
-  description: "Annotates the story with semantic info. Optionally, put ABOVE readmspa in the load order so readmspa runs first.",
+  description: "Annotates the story with semantic info. Optionally, put ABOVE readmspa in the load order so readmspa runs first and this builds on it.",
   author: "GiovanH",
-  version: 0.1,
+  version: 0.3,
 
   computed(api_) {
     api = api_
-    if (api.store.get("clearcache")) {
-      api.logger.info("Got clearcache: Clearing cache and unsetting clearcache flag")
+    if (NOCACHE || api.store.get("clearcache")) {
+      api.logger.info("Got clearcache/nocache: Clearing cache and unsetting clearcache flag")
       api.store.set("cache", {})
       api.store.set("clearcache", false)
     }
@@ -367,7 +420,7 @@ module.exports = {
         })
       }
       // api.logger.info(page_num, archive.footnotes.story[page_num])
-      if (archive.footnotes.story[page_num]) {
+      if (archive.footnotes && archive.footnotes.story[page_num]) {
         for (const i in archive.footnotes.story[page_num]) {
           const scraped_info = makeSemanticContent(archive.footnotes.story[page_num][i].content, {
             page: page_num,
@@ -422,6 +475,7 @@ module.exports = {
                 :disabled="wouldBeConvosMap[person] == false"
               />
               <span v-text="person" :style="personColor(person)" />
+              <!--<span v-text="personToTag(person)" :style="personColor(person)" />-->
             </label>
           </li>
         </ul>
@@ -530,7 +584,7 @@ module.exports = {
           }
         },
         computed: {
-          convoPages() {return Object.values(this.$archive.mspa.story).filter(p => p.sem_participants)},
+          convoPages() {return Object.values(this.$archive.mspa.story).filter(p => p.sem_participants?.length)},
           allPeople() {return Object.keys(this.$archive.flags['mod.semantic'].all_people)},
           selectedPersonsList(){
             return Object.keys(this.selectedPersonsDict).filter(k => this.selectedPersonsDict[k])
@@ -539,12 +593,25 @@ module.exports = {
             return this.m_wouldBeConvosMap(this.selectedPersonsList, this.includePartial)
           },
         },
+        mounted() {
+          window.Semantic = this
+        },
         methods: {
           convoMatches(person_list) {
             return this.convoPages
               .filter(p => person_list.every(
                 v => this.effectiveParticipants(p.sem_participants).has(v)
               ))
+          },
+          personToTag(person) {
+            const matcher = person_matchers.filter(m => m.name == person)[0]
+            if (matcher.tag) {
+              return matcher.tag
+            }
+            let tag = matcher.name
+            tag = tag.replace('.sprite', 'sprite')
+            tag = tag.replace('.', '_')
+            return tag
           },
           effectiveParticipants(particpants_list) {
             return this.p_effectiveParticipants(particpants_list, this.includePartial)
